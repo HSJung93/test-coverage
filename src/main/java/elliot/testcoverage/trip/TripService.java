60eedb516569965758c3b67fb6a03c0702645f67
1113193 <jrps2212@gmail.com>
2022-03-01 23:54:30 +0900

    TripService refactored

--- src/main/java/elliot/testcoverage/trip/TripService.java
+++ src/main/java/elliot/testcoverage/trip/TripService.java
@@ -9,25 +9,17 @@ import java.util.List;
 public abstract class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
-
-        // scene -> division of 2 classes
-        User loggedUser = getLoggedInUser();
-        boolean isFriend = false;
-        if(loggedUser != null){
-            for(User friend:user.getFriends()){
-                if(friend.equals(loggedUser)){
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend){
-                tripList = tripsBy(user);
-            }
-            return tripList;
-        }else{
+        if (getLoggedInUser() == null){
             throw new UserNotLoggedInException();
         }
+
+        return user.isFriendsWith(getLoggedInUser())
+            ? tripsBy(user)
+            : noTrips();
+    }
+
+    private ArrayList<Trip> noTrips() {
+        return new ArrayList<Trip>();
     }
 
     protected List<Trip> tripsBy(User user) {
